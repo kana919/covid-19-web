5f80df8d6ebb8eec8201e303572583c489f1f55c
satoshi03 <innamisatoshi@gmail.com>
2020-05-14 14:00:36 +0900

    Add pref table

--- js/src/App.js
+++ js/src/App.js
@@ -1,18 +1,43 @@
+import moment from 'moment';
+
+
 export class App {
   constructor() {
+    moment.locale('ja');
+    this.apiDomain = 'http://localhost:8000/';
+    this.japanInfectionInfo = {};
+    this.prefInfectionInfo = {};
+    this.behaviorInfo = {};
   }
 
   mount() {
     try {
-      // 感染情報を取得
-      const infectionInfo = fetchFromAPI(`http://localhost:8000/stats/infection`);
-      infectionInfo.then(infectionInfo => {
-        displayTotalNum(infectionInfo);
+      // 日本の感染情報を取得
+      this.japanInfectionInfo = fetchFromAPI(`${this.apiDomain}/stats/infection/japan`);
+      this.japanInfectionInfo.then(infectionInfo => {
+        displayJapanStats(infectionInfo);
       });
 
       // 行動情報を取得
-      const behaviorInfo = fetchFromAPI(`http://localhost:8000/stats/behavior`);
-      behaviorInfo.then(behaviorInfo => {
+      this.behaviorInfo = fetchFromAPI(`${this.apiDomain}/stats/behavior`);
+      this.behaviorInfo.then(behaviorInfo => {
+        displayBehaviorStats(behaviorInfo);
+      });
+
+      // 都道府県ごとの感染情報を取得
+      this.prefInfectionInfo = fetchFromAPI(`${this.apiDomain}/stats/infection/prefectures`);
+      this.prefInfectionInfo.then(infectionInfo => {
+        // 東京都の感染情報の概要表示
+        displayPrefectureStatsOverview(infectionInfo[12]);
+
+        // イベントリスナーの登録
+        for (let info of infectionInfo) {
+          let prefCellElement = document.querySelector(`#${info.name_en}`);
+          prefCellElement.addEventListener("mouseover", {info: info, handleEvent: handleEventDisplayStats});
+        }
+
+        // 各都道府県の感染情報テーブル表示
+        displayPrefectureStats(infectionInfo);
       });
     } catch (error) {
       console.error(`${error}`);
@@ -34,59 +59,83 @@ function fetchFromAPI(url) {
   });
 }
 
-function calcJapanTotalNum(infectionInfo) {
-  let total_infected = 0;
-  let total_recovered = 0;
-  let total_dead = 0;
-  for (const info of infectionInfo) {
-    total_infected += info.total_infected;
-    total_recovered += info.total_recovered;
-    total_dead += info.total_death;
+function displayJapanStats(infectionInfo) {
+  // TODO: 修正
+  if (infectionInfo.length === 0) {
+    return
   }
-  return {
-    "infected": total_infected,
-    "recovered": total_recovered,
-    "dead": total_dead
-  };
+
+  // 日時見出し
+  const dateElement = document.querySelector("#date");
+
+  dateElement.innerHTML = moment(infectionInfo[0].reported_date).format('MMMMDo(dddd)');
+
+  // 当日
+  const infectionNumElement = document.querySelector("#jp-infection-num");
+  const recoveryNumElement = document.querySelector("#jp-recovery-num");
+  const deadNumElement = document.querySelector("#jp-dead-num");
+
+  infectionNumElement.innerHTML = infectionInfo[0].current_infected + '人';
+  recoveryNumElement.innerHTML = infectionInfo[0].total_recovered - infectionInfo[1].total_recovered + '人';
+  deadNumElement.innerHTML = infectionInfo[0].total_death - infectionInfo[1].total_death + '人';
+
+  // 累計
+  const totalInfectionNumElement = document.querySelector("#jp-total-infection-num");
+  const totalRecoveryNumElement = document.querySelector("#jp-total-recovery-num");
+  const totalDeadNumElement = document.querySelector("#jp-total-dead-num");
+
+  totalInfectionNumElement.innerHTML = infectionInfo[0].total_infected + '人';
+  totalRecoveryNumElement.innerHTML = infectionInfo[0].total_recovered + '人';
+  totalDeadNumElement.innerHTML = infectionInfo[0].total_death + '人';
 }
 
-function calcJapanLatestNum(infectionInfo) {
-  let total_infected = 0;
-  let total_recovered = 0;
-  let total_dead = 0;
-  for (const info of infectionInfo) {
-    if (info.daily && info.daily[0]) {
-      total_infected += info.daily[0].infected;
-      total_recovered += info.daily[0].recovered;
-      total_dead += info.daily[0].death;
+function displayPrefectureStats(infectionInfo) {
+  // 感染者数テーブル
+  for (let info of infectionInfo) {
+    let prefCellElement = document.querySelector(`#${info.name_en}`);
+    if (info.daily.length > 0) {
+      // データが存在している場合は現在感染者数を表示
+      prefCellElement.innerHTML = info.daily[0].current_infected;
+    } else {
+      // データがない場合は不明
+      prefCellElement.innerHTML = '不明';
     }
   }
-  return {
-    "infected": total_infected,
-    "recovered": total_recovered,
-    "dead": total_dead
-  };
 }
 
-
-function displayTotalNum(infectionInfo) {
-  const jpInfectionNumElement = document.querySelector("#jp-infection-num");
-  const jpTotalInfectionNumElement = document.querySelector("#jp-total-infection-num");
-  const jpRecoveryNumElement = document.querySelector("#jp-recovery-num");
-  const jpTotalRecoveryNumElement = document.querySelector("#jp-total-recovery-num");
-  const jpDeadNumElement = document.querySelector("#jp-dead-num");
-  const jpTotalDeadNumElement = document.querySelector("#jp-total-dead-num");
-
-  const total = calcJapanTotalNum(infectionInfo);
-  jpTotalInfectionNumElement.innerHTML = total.infected + '人';
-  jpTotalRecoveryNumElement.innerHTML = total.recovered + '人';
-  jpTotalDeadNumElement.innerHTML = total.dead + '人';
-
-  const latest = calcJapanLatestNum(infectionInfo);
-  jpInfectionNumElement.innerHTML = latest.infected + '人';
-  jpRecoveryNumElement.innerHTML = latest.recovered + '人';
-  jpDeadNumElement.innerHTML = latest.dead + '人';
+function displayBehaviorStats(behaviorInfo) {
+  const stayHomeElement = document.querySelector("#jp-stay-home-ratio");
 }
 
-function displayInfectionTable(infectionInfo) {
+function handleEventDisplayStats(e) {
+  displayPrefectureStatsOverview(this.info);
+}
+
+function displayPrefectureStatsOverview(info) {
+  const prefNameElement = document.querySelector('#prefecture-name');
+  prefNameElement.innerHTML = info.name;
+
+  //
+  const infectionNumElement = document.querySelector("#infection-num");
+  const recoveryNumElement = document.querySelector("#recovery-num");
+  const deadNumElement = document.querySelector("#dead-num");
+
+  const totalInfectionNumElement = document.querySelector("#total-infection-num");
+  const totalRecoveryNumElement = document.querySelector("#total-recovery-num");
+  const totalDeadNumElement = document.querySelector("#total-dead-num");
+
+  if (info.daily.length > 0) {
+    // データが存在している場合は現在感染者数を表示
+    infectionNumElement.innerHTML = info.daily[0].current_infected + '人';
+    recoveryNumElement.innerHTML = info.total_recovered + '人';
+    deadNumElement.innerHTML = info.total_death + '人';
+    totalInfectionNumElement.innerHTML = info.daily[0].current_infected + '人';
+    totalRecoveryNumElement.innerHTML = info.total_recovered + '人';
+    totalDeadNumElement.innerHTML = info.total_death + '人';
+  } else {
+    // データがない場合は不明
+    infectionNumElement.innerHTML = '- 人';
+    recoveryNumElement.innerHTML = '- 人';
+    deadNumElement.innerHTML = '- 人';
+  }
 }
